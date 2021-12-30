58f8d40fc0ac779c43f1a5cd40beb3e4d1bd9754
root <sudak-91>
2021-12-30 21:27:21 +0300

    On branch master  Changes to be committed: 	modified:   example/simple_server.go 	new file:   example/tetsingcode.go 	modified:   utility.go

--- example/simple_server.go
+++ example/simple_server.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/sudak-91/engcore_modbus"
 )
 
 func main() {
 	mbMap := engcore_modbus.NewModbusMap()
 	ModBusServer := engcore_modbus.NewModbusServer("0.0.0.0/24", 5002, mbMap)
-	go ModBusServer.StartServer()
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM)
-	<-quit
+	ModBusServer.StartServer()
+	//go ModBusServer.StartServer()
+	//quit := make(chan os.Signal, 1)
+	//signal.Notify(quit, syscall.SIGTERM)
+	//<-quit
 }
--- utility.go
+++ utility.go
@@ -23,11 +23,12 @@ func GetByteCount(data []byte) uint16 {
 	return binary.BigEndian.Uint16(result)
 }
 
+//Check offset and name length
 func ValidateSetNameData(offset int, name string) error {
 	if offset > 65535 {
 		return fmt.Errorf("coil outside")
 	}
-	if name == "" {
+	if len(name) == 0 {
 		return fmt.Errorf("empty name")
 	}
 	return nil
